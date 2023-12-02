8fdf464d6a3bc6545905c99039d16084e2f1a98c
Mohammad Mahdi Sabooni <apex.m.scott.kim@gmail.com>
2023-12-02 23:18:33 +0330

    Delete Exercise 3.py

--- Exercise 3.py
+++
@@ -1,7 +0,0 @@
-num1, num2, num3 = input('Please enter 3 numbers separated by "," = ').split(',')
-num1a = int(num1)
-num2a = int(num2)
-num3a = int(num3)
-nums = [num1a, num2a, num3a]
-nums.sort(reverse=True)
-print('The second largest number = ', nums[1])
