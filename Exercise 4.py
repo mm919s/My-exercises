8c8aeebbaa89be95b1284a6aceb4eddb8d21d778
Mohammad Mahdi Sabooni <apex.m.scott.kim@gmail.com>
2023-12-02 23:08:32 +0330

    Delete Exercise 4.py

--- Exercise 4.py
+++
@@ -1,5 +0,0 @@
-r = int(input("r = ")) #Receives r
-import math #Imports math library
-p2 = 2 * math.pi * r #Gets and names the the circumference of the circle
-s = (r ** 2) * math.pi #Gets and names the area of the circle
-print("2P = ",p2,",","S = ",s) #Prints the circumference and area of the circle due 'r' is considered as the radius of the circle
