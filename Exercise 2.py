f736ffc6beb5b160e6437ba1c0dbe5f09bc50db7
Mohammad Mahdi Sabooni <apex.m.scott.kim@gmail.com>
2023-12-02 23:08:16 +0330

    Delete Exercise 2.py

--- Exercise 2.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Num 1 = ")) #Receives a
-b = float(input("Num 2 = ")) #Receives b
-c = float(input("Num 3 = ")) #Receives c
-a1 = a ** 3 #Names the variable a to the power of 3
-b1 = b ** 3 #Names the variable b to the power of 3
-c1 = c ** 3 #Names the variable cto the power of 3
-sum = a1 + b1 + c1 #Names the sum of variables a,b and c
-print("sum = ",sum) #Prints the value of sum variable
