8e37ac46617986e905b3efa7930ad422cd533112
Mohammad Mahdi Sabooni <apex.m.scott.kim@gmail.com>
2023-12-02 22:58:04 +0330

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,10 @@
+a = float(input("a = ")) #Receives a
+b = float(input("b = ")) #Receives b
+c = float(input("c = ")) #Receives c
+import math #Imports math library
+d = a - b #Names the value of variable a minus variable b
+e = math.fabs(d) #Names the absolute value of variable d
+f = math.fabs(c) #Names the absolute value of variable c
+g = math.pow(e,f) #Names the variable e to the power of variable f 
+sum = math.sqrt(g) #Names the root of variable g
+print("sum = ",sum) #Prints the value of variable sum
