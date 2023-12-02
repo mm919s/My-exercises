10f5b8d34c6e09b9e9fde539db7d6196539a75d2
Mohammad Mahdi Sabooni <apex.m.scott.kim@gmail.com>
2023-12-02 23:08:24 +0330

    Delete Exercise 3.py

--- Exercise 3.py
+++
@@ -1,5 +0,0 @@
-height = float(input("Height(cm) = ")) #Receives height(cm)
-weight = float(input("Weight(g) = ")) #Receives weight(g)
-height1 = height / 100 #Names the value of variable height divided by 100
-weight1 = weight / 1000 #Names the value of variable weight divided by 1000
-print("Height(m) = ",height1,",","Weight(kg) = ",weight1) #Prints the variable height1 as height(m) and variable weight1 as weight(kg)
